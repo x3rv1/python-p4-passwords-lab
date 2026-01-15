e53f29a03dc171d6f30e4f45fe780d6ec44cdfd7
Peter Xervi <xervipeter@gmail.com>
2026-01-15 10:12:53 +0300

    done

--- server/app.py
+++ server/app.py
@@ -25,19 +25,41 @@ class Signup(Resource):
         user.password_hash = json['password']
         db.session.add(user)
         db.session.commit()
+        session['user_id'] = user.id
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        return {}, 204
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter(User.username == json.get('username')).first()
+
+        if user and user.authenticate(json.get('password')):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {'error': 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
